0420aec19d99fbfe6da3c3d789e578fc448e791c
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2017-05-16 17:09:24 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,8 +2,8 @@ variable "aws_region" {
 	default = "us-west-2"
 }
 variable "aws_access_key" {
-	default = "AKIAJAJ4SMTDZNLDKCYQ"
+	default = "AKIAJ*******************CYQ"
 }
 variable "aws_secret_key" {
-	default = "VoodzqGtpk3lDYwEED7Q0RFZajJ/Wai+0DUTHyQP"
+	default = "Vood****************************yQP"
 }
